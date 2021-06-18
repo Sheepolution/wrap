8d5887df15762aef6727ebc683eeef7fa5ea7e35
Sheepolution <danielhaazen@hotmail.com>
2021-06-18 17:45:34 +0200

    Add support for chaining

--- README.md
+++ README.md
@@ -46,6 +46,25 @@ Which is equal to this:
 f = function () self.bar = 10 end
 ```
 
+### Chaining
+
+By using `wrap:chain()` you can chain multiple methods. Finish the chain with `:done()`.
+
+```lua
+f = self.wrap:chain():foo()({bar = 10, test = "example"}):foobar("Hello world"):done()
+```
+
+Which is equal to this:
+
+```lua
+f = function ()
+    self:foo()
+    self.bar = 10
+    self.test = "example"
+    self:foobar("Hello world")
+end
+```
+
 ## License
 
 This library is free software; you can redistribute it and/or modify it under the terms of the MIT license. See [LICENSE](LICENSE) for details.
--- wrap.lua
+++ wrap.lua
@@ -1,8 +1,7 @@
 local wrap = {}
-wrap.__index = wrap
 
 function wrap.new(obj)
-	return setmetatable({obj = obj}, wrap)
+	return setmetatable({obj = obj, chain = wrap.chain}, wrap)
 end
 
 function wrap:__index(f)
@@ -14,12 +13,48 @@ function wrap:__index(f)
 	end
 end
 
-function wrap:__call(t)
+function wrap:__call(s, t)
+	if not t then t = s s = self.obj end
 	return function ()
 		for k,v in pairs(t) do
-			self.obj[k] = v
+			s[k] = v
 		end
 	end
+end
+
+local chain_mt = {}
+
+function chain_mt:__index(f)
+	return function (obj, ...)
+		local a = {...}
+		self[#self + 1] = function ()
+			return self.obj[f](self.obj, unpack(a))
+		end
+		return self
+	end
+end
+
+function chain_mt:__call(s,t)
+	if not t then t = s s = self.obj end
+	self[#self + 1] = function ()
+		for k,v in pairs(t) do
+			s[k] = v
+		end
+	end
+	return self
+end
+
+
+function chain_mt:done ()
+	return function ()
+		for i,v in ipairs(self) do
+			v()
+		end
+	end
+end
+
+function wrap:chain()
+	return setmetatable({obj = self.obj, done = chain_mt.done }, chain_mt)
 end
 
 return wrap
